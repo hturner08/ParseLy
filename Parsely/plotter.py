d0d6dc921ae5160acc844437a798da852d13c566
Herbert Turner <hmturner@mit.edu>
2019-08-09 18:15:27 -0400

    Testing with reinstalled version of VisIt

--- Parsely/plotter.py
+++ Parsely/plotter.py
@@ -1,7 +1,7 @@
 import sys#WHY WONT YOU FUCKING WORK
-sys.path.append(r"C:\Users\Herbert Turner\AppData\Local\Programs\LLNL\VisIt 3.0.0\lib\site-packages")
+sys.path.append(r"C:\Program Files\LLNL\VisIt 3.0.0\lib\site-packages")
 import visit
-visit.Launch(r"C:\Users\Herbert Turner\AppData\Local\Programs\LLNL\VisIt 3.0.0")
+visit.Launch(r"C:\Program Files\LLNL\VisIt 3.0.0")
 print("Hello")
 visit.OpenDatabase(r"C:\Users\Herbert Turner\Documents\SparcCode\MeshtalParser\Parsely\bin\output.txt")
 print("FUCKING WORK")
