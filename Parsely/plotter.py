23a4859d4323945308e0d8592e1a554cbd593c9e
Herbert Turner <hmturner@mit.edu>
2019-08-11 23:57:31 -0400

    Fixed python plotter issue. Requires python 2.7

--- Parsely/plotter.py
+++ Parsely/plotter.py
@@ -1,13 +1,14 @@
 import sys#WHY WONT YOU FUCKING WORK
 sys.path.append(r"C:\Program Files\LLNL\VisIt 3.0.0\lib\site-packages")
 import visit
-visit.Launch(r"C:\Program Files\LLNL\VisIt 3.0.0")
+visit.Launch()
 print("Hello")
-visit.OpenDatabase(r"C:\Users\Herbert Turner\Documents\SparcCode\MeshtalParser\Parsely\bin\output.txt")
+visit.OpenDatabase(r"C:\Users\Herbert Turner\Documents\SparcCode\MeshtalParser\Parsely\bin\output2.txt")
 print("FUCKING WORK")
-visit.AddPlot("Scatter", "u")
-p = ScatterPlotAttributes()
+visit.AddPlot("Scatter",["var00","var01","var02","var03"])
+p = ScatterAttributes()
 p.colorTableName = "hot"
 p.opacity = 0.5
 SetPlotOptions(p)
+print("Checkpoint3")
 DrawPlots()
